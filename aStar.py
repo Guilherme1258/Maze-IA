ff4e91a71907289b65e5c036462507f0c5b1a151
Guilherme1258 <selio.filho@aluno.ufop.edu.br>
2023-05-28 20:55:01 -0300

    Terminou

--- aStar.py
+++ aStar.py
@@ -1,30 +1,52 @@
-def AStar(labirinto):
-    fronteira = []
-    caminhos = []
-    celulasVisitadas = []
-    statusLabirinto = []
+from pyamaze import maze,agent,textLabel
+from queue import PriorityQueue
 
-    fronteira.append(labirinto.getPosicaoInicio())
+def h(cell1,cell2):
+    x1,y1 = cell1
+    x2,y2 = cell2
 
-    while fronteira != []:
-        # Imprime Fronteira
-        statusLabirinto.append(labirinto.getLabirintoPlot(posicoes=fronteira, cores=(0,255,127)))
+    return abs(x1-x2) + abs(y1-y2)
 
-        celula = fronteira.pop(0) # Selecionando o primeiro elemento da lista
-        celulasVisitadas.append(celula)
-        caminhos.append(celula)
+def aStar(labirinto):
+    inicio = (labirinto.rows,labirinto.cols)
+    g_score = {cell:float('inf') for cell in labirinto.grid}
+    g_score[inicio] = 0
+    f_score = {cell:float('inf') for cell in labirinto.grid}
+    f_score[inicio] = h(inicio,labirinto._goal)
 
-        if celula == labirinto.getDestino():
-            print("Objetivo", celula, "encontrado")
-            caminhoEncontrado = selecionaCaminho(caminhos, labirinto.getDestino())
-            statusLabirinto.append(labirinto.getLabirintoPlot(posicoes=caminhoEncontrado, cores=(0,0,255)))
-            return statusLabirinto
+    open = PriorityQueue()
+    open.put((h(inicio, labirinto._goal), h(inicio,labirinto._goal), inicio))#
+    aPath = {}
 
-        else:
-            for vizinho in getVizinhos(celula, labirinto):
-                if vizinho not in celulasVisitadas:
-                    fronteira.append(vizinho)
-                    
-                    fronteira.sort(key=lambda x: abs(x[0]-labirinto.getDestino()[0])+abs(x[1]-labirinto.getDestino()[1]))
+    while not open.empty():
+        currCell = open.get()[2]
 
-    print("Objetivo nao existente no grafo")
+        if currCell == labirinto._goal:
+            break
+        
+        for d in 'ESNW':
+            if labirinto.maze_map[currCell][d] == True:
+                if d == 'E':
+                    childCell = (currCell[0],currCell[1]+1)
+                if d == 'W':
+                    childCell = (currCell[0],currCell[1]-1)
+                if d == 'N':
+                    childCell = (currCell[0]-1,currCell[1])
+                if d == 'S':
+                    childCell = (currCell[0]+1,currCell[1])
+
+                temp_g_score = g_score[currCell]+1
+                temp_f_score  = temp_g_score+h(childCell, labirinto._goal)
+
+                if temp_f_score  < f_score[childCell]:
+                    g_score[childCell] =  temp_g_score
+                    f_score[childCell] =  temp_f_score 
+                    open.put((temp_f_score,h(childCell,labirinto._goal),childCell))
+                    aPath[childCell] = currCell
+    fwdPath = {}
+    cell = labirinto._goal
+
+    while cell != inicio:
+        fwdPath[aPath[cell]] = cell
+        cell = aPath[cell]
+    return fwdPath
